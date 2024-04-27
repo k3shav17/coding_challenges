e251063687da21d7f78e52fa8e26d02537c3f16b
Keshava <keshavrao0489@gmail.com>
2024-04-27 17:16:16 +0530

    unable to read from std in

--- cut_challenge_four/go_cut.go
+++ cut_challenge_four/go_cut.go
@@ -12,16 +12,16 @@ var delimiter = "\t"
 
 func main() {
 
-	var lines []string
+	var dataForLines []string
 	commandLineArgs := os.Args[1:]
 	flag := commandLineArgs[0]
 
 	splitFunc := func(c rune) bool {
 		return c == ' ' || c == ','
 	}
-	lines = strings.FieldsFunc(flag[2:], splitFunc)
+	dataForLines = strings.FieldsFunc(flag[2:], splitFunc)
 	var cols []int
-	for _, val := range lines {
+	for _, val := range dataForLines {
 		col, err := strconv.Atoi(val)
 		if err != nil {
 			fmt.Println("Please enter a column or line to cut")
@@ -30,8 +30,9 @@ func main() {
 		cols = append(cols, col)
 	}
 
-	filePath := commandLineArgs[1]
-	if len(commandLineArgs) >= 3 {
+	filePath := commandLineArgs[1] // if there is no delimiter
+
+	if len(commandLineArgs) >= 3 { // if there is a delimiter
 		filePath = commandLineArgs[2]
 		delimiter = commandLineArgs[1][len(commandLineArgs[1])-1:]
 	}
@@ -40,15 +41,12 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer content.Close()
-
 	processFile(content, cols)
+	defer content.Close()
 }
 
 func processFile(fileContent *os.File, cols []int) {
-
 	scanner := bufio.NewScanner(fileContent)
-
 	for scanner.Scan() {
 		line := scanner.Text()
 		doCutFields(line, delimiter, cols)
@@ -56,9 +54,7 @@ func processFile(fileContent *os.File, cols []int) {
 }
 
 func doCutFields(line, delimiter string, fields []int) {
-
 	tokens := strings.Split(line, delimiter)
-
 	for _, col := range fields {
 		if col <= len(tokens) {
 			fmt.Print(tokens[col-1], " ")
